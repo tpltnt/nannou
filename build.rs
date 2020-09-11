eca610c0cd485e941f0a1ea48d72b36257577085
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-09-11 12:02:28 +0200

    skeleton for platform specific gstreamer installation

--- build.rs
+++ build.rs
@@ -1,2 +1,18 @@
-fn main() {}
+#[cfg(target_os = "linux")]
+fn install_gstreamer() {
+    println!("linux");
+}
 
+#[cfg(target_os = "macos")]
+fn install_gstreamer() {
+    println!("no automatic gstreamer installation");
+}
+
+#[cfg(target_os = "windows")]
+fn install_gstreamer() {
+    println!("no automatic gstreamer installation");
+}
+
+fn main() {
+    install_gstreamer();
+}
