55198bfd6d5a74f4847f8d96bf944082f7fc41ad
tpltnt <1172976+tpltnt@users.noreply.github.com>
2020-09-11 12:41:15 +0200

    detecting different Linux distributions via package manager

--- build.rs
+++ build.rs
@@ -1,6 +1,37 @@
+use std::process::Command;
+
 #[cfg(target_os = "linux")]
 fn install_gstreamer() {
-    println!("linux");
+    println!("Linux detected");
+    // Fedora / Red Hat
+    if Command::new("which")
+        .args(&["dnf"])
+        .status()
+        .unwrap()
+        .success()
+    {
+        println!("Fedora detected via dnf");
+    }
+
+    // Debian / Ubuntu
+    if Command::new("which")
+        .args(&["apt"])
+        .status()
+        .unwrap()
+        .success()
+    {
+        println!("Debian detected via apt");
+    }
+
+    // Arch Linux
+    if Command::new("which")
+        .args(&["pacman"])
+        .status()
+        .unwrap()
+        .success()
+    {
+        println!("Arch detected via pacman");
+    }
 }
 
 #[cfg(target_os = "macos")]
